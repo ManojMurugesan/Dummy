759877e572ade0dd038cb15e9f02d94f2e69a9b2
Manoj <manojmurugesan43@gmail.com>
2024-09-17 18:01:35 +0530

    First commit

---
+++ src/main/java/GitXDemo/GitXDemo/App.java
@@ -0,0 +1,15 @@
+package GitXDemo.GitXDemo;
+
+public class App {
+	
+	public void Demo()
+	{
+		System.out.println("Print Demo");
+	}
+    public static void main(String[] args) {
+    	
+    	App ob = new App();
+    	ob.Demo();
+        System.out.println("Hello World!");
+    }
+}
